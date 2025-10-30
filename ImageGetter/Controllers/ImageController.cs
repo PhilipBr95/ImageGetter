9ba5d3fb7e685606a08a8ecd64d80ede779d10e7
Philip Bridges <philipbr95@gmail.com>
2025-10-30 01:16:41 +0000

    ..

--- ImageGetter/Controllers/ImageController.cs
+++ ImageGetter/Controllers/ImageController.cs
@@ -102,7 +102,7 @@ namespace ImageGetter.Controllers
                 Face? face = await FindFace(file);
 
                 //Did we find a face with reasonable confidence?
-                if (face!.Confidence > 2)
+                if (face!.Confidence > 5)
                 {
                     _logger.LogDebug($"Face found at {face.X},{face.Y} size {face.Width}x{face.Height} with {face.Confidence} Confidence");
                     resizeOptions.CenterCoordinates = new PointF(face.X + (face.Width / 2), face.Y + (face.Height / 2));
