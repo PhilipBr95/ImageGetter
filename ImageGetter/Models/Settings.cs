0508af3404edd1a8a1fb337af7d503416a58aa66
Phil <PB@Bettingaid.com>
2025-12-01 21:27:47 +0000

    more folders

--- ImageGetter/Models/Settings.cs
+++ ImageGetter/Models/Settings.cs
@@ -5,7 +5,7 @@
         public string Host { get; set; } = "egypt";
         public string? ImagePassword { get; set; }
         public string Username { get; set; } = "ImageGetter";
-        public IEnumerable<string> Paths { get; set; } = ["/photo/Phil's Phone", "/photo/Gill's Phone"];
+        public IEnumerable<string> Paths { get; set; } = ["/photo/Phil's Phone", "/photo/Gill's Phone", "/photo/Gill's Selective Phone", "/photo/Camera Selective Photos"];
         public string? GoogleApiKey { get; internal set; }
         public string? FaceApi { get; set; } = "http://192.168.1.116:8092"; //To dev locally 5000
         public double MinConfidence { get; set; } = 50000;
